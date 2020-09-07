dd98519555b3de58418cc240504bb06ad2d1596a
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-09-07 12:59:00 +0530

    feat: adds header fro the app and changes the version of eslint according to the react comaptibility

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/jsx-indent */
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <h1>Movie Cards</h1>
+    </div>
+  );
+};
+
+export default Header;
