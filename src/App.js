3803a1abe7c2aa0f93045a46f2146fab7acf197b
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-09-09 12:09:28 +0530

    feat: Implements nav buttons to route through home page and about page, installed react router dom modules for routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,45 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Cards from './components/Cards';
+import About from './components/About'
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 const App = () =>  { 
 
     return (
-      <div>
-        <Header />
-        <Cards />        
-      </div>     
+      <Router>
+        <div>
+          <div className= 'nav-container'>
+            <Link to="/">
+              <button  className='header-btn'>Home</button>
+            </Link>  
+
+            <Link to="/about">
+              <button  className='header-btn'>About</button>
+              </Link>
+          </div>
+
+          <Switch>
+
+            <Route exact path="/about">
+              <About />
+            </Route>
+
+            <Route path="/">
+              <Header />
+              <Cards />
+            </Route>
+            
+          </Switch>          
+        </div> 
+      </Router>
+          
     );
     
   }
